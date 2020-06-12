fc19ec4e9b25589a8ba4933242877d61405c25db
Jaderson Alex <jadersonalex@gmail.com>
2020-06-12 19:34:02 -0300

    primeiro commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameController : MonoBehaviour
+{
+    public bool  GameOver = false;
+private float ContagemFim = 3;
+public int Score;
+public Text scoreText;
+    void Start(){
+        Time.timeScale = 1;
+    }
+        
+        private void FixedUpdate()
+        {
+            if(GameOver) return;
+        }
+    void update()
+    {
+        if(GameOver)
+        {
+            ContagemFim -= Time.deltaTime;
+            if(ContagemFim<=0)
+            {
+                if(Input.GetKeyDown(KeyCode.Space)|| Input.GetMouseButtonDown(0))
+                {
+                   SceneManager.LoadScene("SampleScene");
+                }
+            }
+        }
+    }
+      public void RestartGame(){
+          SceneManager.LoadScene(0);
+      }
+ private void OnCollisionEnter(Collision col) {
+     GameOver = true;
+ }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject MenuPrincipal;
+
+    private void Start()
+    {
+          Time.timeScale = 0;
+    }
+    public void Jogar()
+    {
+        Time.timeScale = 1;
+        //transform.getchild(0).gameobject.setActive(false);
+        MenuPrincipal.SetActive(false);   
+    }
+
+
+      public  void Sair()
+    {
+        Application.Quit();
+    }
+}
